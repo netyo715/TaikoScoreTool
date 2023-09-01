296c59e46964ce30aaad635bf98b6cd84bf73779
Sohei Nishimura <nishimura715@gmail.com>
2023-09-01 18:15:57 +0900

    複数のジャンルを持つ曲の正常な読み込み 楽曲情報追加 難易度表示/フィルター/ソート

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import ScoreTable from './ScoreTable';
+
 function App() {
   const songList = [{id:1, name:"aiueo", crown:1, rank:1}, {id:2, name:"kakikukeko", crown:0, rank:2}, {id:3, name:"bakikukeko", crown:3, rank:7}];
   return (
     <div className="App">
+      <p>「このコンテンツはファンメイドコンテンツです。ファンメイドコンテンツポリシー（<a href="https://taiko-ch.net/ip_policy/">https://taiko-ch.net/ip_policy/）のもと制作されています。」</a></p>
       <ScoreTable songList={songList}></ScoreTable>
     </div>
   );
--- src/ScoreTable.tsx
+++ src/ScoreTable.tsx
@@ -12,28 +12,53 @@ import TableBody from '@mui/material/TableBody';
 import Paper from '@mui/material/Paper';
 import { Button } from '@mui/material';
 
+import SongInformation from "./songInformation.json";
+
 interface SongData {
   id: number;
   name: string;
   crown: number;
   rank: number;
-}
+};
 
 interface Props {
   songList: SongData[];
-}
+};
 
 const CROWN = ["未クリア", "クリア", "フルコンボ", "ドンダフルコンボ"];
 const RANK = ["ランクなし", "粋1", "粋2", "粋3", "雅1", "雅2", "雅3", "極"];
 
 const ScoreTable: React.FC<Props> = (props) => {
+  const [filterDifficulty, setFilterDifficulty] = useState<boolean[]>(new Array(11).fill(true));
   const [filterCrown, setFilterCrown] = useState<boolean[]>(new Array(4).fill(true));
   const [filterRank, setFilterRank] = useState<boolean[]>(new Array(9).fill(true));
-  const [sortBy, setSortBy] = useState<'id' | 'name' | 'crown' | 'rank'>('id');
+  const [sortBy, setSortBy] = useState<'id' | 'name' | 'crown' | 'rank' | 'difficulty'>('id');
   const [sortDescending, setSortDescending] = useState<boolean>(false);
-  const [songList, setSongList] = useState<{id:number, name:string, crown:number, rank:number}[]>([]);
+  const [songList, setSongList] = useState<{id:number, name:string, difficulty: number, crown:number, rank:number}[]>([]);
+  const [songInformation, setSonginformation] = useState<{[key: string]: SongInformationValue}[]>([{}, {}]);
+
+  type SongInformationValue = {
+    id: number;
+    mainGenre: number;
+    genre: number[];
+    difficulty: number;
+  };
 
   useEffect(() => {
+    var songInformationTemp: {[key: string]: SongInformationValue}[] = [{}, {}]
+    SongInformation.map((value) => {
+      const index = value.isUra ? 1 : 0;
+      var genre = [value.genre1];
+      if (value.genre2 != 0){
+        genre.push(value.genre2)
+      }
+      if (value.genre3 != 0){
+        genre.push(value.genre3)
+      }
+      songInformationTemp[index][value.songName] = {id: value.id, mainGenre: value.mainGenre, genre: genre, difficulty: value.difficulty};
+    });
+    setSonginformation(songInformationTemp);
+
     const storageData = localStorage.getItem("songList");
     if (storageData){
       setSongList(JSON.parse(storageData));
@@ -42,6 +67,12 @@ const ScoreTable: React.FC<Props> = (props) => {
       console.log("localstorage score not found.");
     }
   }, []);
+
+  const toggleFilterDifficulty = (index: number) => {
+    const updatedFilters = [...filterDifficulty];
+    updatedFilters[index] = !updatedFilters[index];
+    setFilterDifficulty(updatedFilters);
+  };
 
   const toggleFilterCrown = (index: number) => {
     const updatedFilters = [...filterCrown];
@@ -73,8 +104,13 @@ const ScoreTable: React.FC<Props> = (props) => {
     }
     var result = [];
     const parsedJson: {songName: string, isUra: boolean, crown: number, rank: number}[] = JSON.parse(data);
+    var added = new Array(2000).fill(false);
     for (var i=0; i<parsedJson.length; i++){
-      result.push({id: i, name: parsedJson[i].songName, crown: parsedJson[i].crown, rank: parsedJson[i].rank});
+      const songInfo = songInformation[parsedJson[i].isUra ? 1: 0][parsedJson[i].songName];
+      if (!added[songInfo.id]){
+        result.push({id: songInfo.id, name: parsedJson[i].songName + (parsedJson[i].isUra ? "(裏)" : ""), difficulty: songInfo.difficulty, crown: parsedJson[i].crown, rank: parsedJson[i].rank});
+        added[songInfo.id] = true;
+      }
     }
     setSongList(result);
     localStorage.setItem("songList", JSON.stringify(result));
@@ -82,7 +118,7 @@ const ScoreTable: React.FC<Props> = (props) => {
   }
 
   const sortedSongs = songList
-    .filter(song => filterCrown[song.crown] && filterRank[song.rank])
+    .filter(song => filterDifficulty[song.difficulty] && filterCrown[song.crown] && filterRank[song.rank])
     .sort((a, b) => {
       const sortMultiplier = sortDescending ? -1 : 1;
       if (sortBy === 'id') {
@@ -91,6 +127,8 @@ const ScoreTable: React.FC<Props> = (props) => {
         return sortMultiplier * a.name.localeCompare(b.name);
       } else if (sortBy === 'crown') {
         return sortMultiplier * (a.crown - b.crown);
+      } else if (sortBy === 'difficulty') {
+        return sortMultiplier * (a.difficulty - b.difficulty);
       } else {
         return sortMultiplier * (a.rank - b.rank);
       }
@@ -102,6 +140,14 @@ const ScoreTable: React.FC<Props> = (props) => {
         <Button onClick={copyScript}>ブックマークレットをコピー</Button>
         <p>適当なWebページをブックマークに登録し、URLをコピーしたブックマークレットに置き換えてください</p>
         <Button onClick={inputScore}>スコア貼り付け</Button>
+      </div>
+      <div>
+        {filterDifficulty.map((checked, index) => (
+          <label key={index}>
+            <Checkbox checked={checked} onChange={() => toggleFilterDifficulty(index)} />
+            {index}
+          </label>
+        ))}
       </div>
       <div>
         {filterCrown.map((checked, index) => (
@@ -122,6 +168,7 @@ const ScoreTable: React.FC<Props> = (props) => {
       <RadioGroup row aria-label="sort" name="sort" value={sortBy} onChange={e => setSortBy(e.target.value as any)}>
         <FormControlLabel value="id" control={<Radio />} label="選曲画面" />
         <FormControlLabel value="name" control={<Radio />} label="曲名" />
+        <FormControlLabel value="difficulty" control={<Radio />} label="難易度" />
         <FormControlLabel value="crown" control={<Radio />} label="王冠" />
         <FormControlLabel value="rank" control={<Radio />} label="ランク" />
       </RadioGroup>
@@ -134,6 +181,7 @@ const ScoreTable: React.FC<Props> = (props) => {
           <TableHead>
             <TableRow>
               <TableCell>曲名</TableCell>
+              <TableCell>難易度</TableCell>
               <TableCell>王冠</TableCell>
               <TableCell>ランク</TableCell>
             </TableRow>
@@ -142,6 +190,7 @@ const ScoreTable: React.FC<Props> = (props) => {
             {sortedSongs.map(song => (
               <TableRow>
                 <TableCell>{song.name}</TableCell>
+                <TableCell>{song.difficulty}</TableCell>
                 <TableCell>{song.crown}</TableCell>
                 <TableCell>{song.rank}</TableCell>
               </TableRow>
